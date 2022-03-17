8a4556390e522346a7eea24638227e862b9a29aa
Jay Manjrekar <89176673+JayManjrekar@users.noreply.github.com>
2022-03-17 15:46:51 +0000

    added better stuff

--- main.py
+++ main.py
@@ -1,47 +1,3 @@
-# Christmas Tree
-def crown(n):
-    for i in range(n):
-        for x in range(n-i):
-            print(' ', end=' ')
-        for y in range(2*i+1):
-            print('*', end=' ')
-        print()
-
-def trunk(n):
-    for x in range(3):
-        for i in range(n-1):
-            print(' ', end=' ')
-        print('* * *')
-
-def christmastree():
-    row = int(input('Enter number of rows: '))
-    crown(row)
-    crown(row)
-    trunk(row)
-
-
-# Ship
-ANSI_CLEAR_SCREEN = u"\u001B[2J"
-ANSI_HOME_CURSOR = u"\u001B[0;0H\u001B[2"
-OCEAN_COLOR = u"\u001B[44m\u001B[2D"
-SHIP_COLOR = u"\u001B[32m\u001B[2D"
-RESET_COLOR = u"\u001B[0m\u001B[2D"
-
-
-def ship_print(position):
-    print(ANSI_HOME_CURSOR)
-    print(RESET_COLOR)
-    sp = " " * position
-    print(sp + "    |\   ")
-    print(sp + "    |/   ")
-    print(SHIP_COLOR, end="")
-    print(sp + "\__ |__/ ")
-    print(sp + " \____/  ")
-    print(RESET_COLOR)
-
-def ship():
-    travel = int(input("Ship Distance? "))
-    ship_print(travel)
 
 # Swap
 def swap():
@@ -57,54 +13,34 @@ def f1():
 def f2():
     print('f2')
 
-def buildMenu(menu):
-    for key,value in menu.items():
-        display = value["display"]
-        print(f"{key} ------ {display}")
-    print("What is your choice? (enter the number value) ")
-
-def presentMenu(menu):
-    buildMenu(menu)
-    choice = int(input())
-    if (choice) in menu:
-        if menu[choice]["type"] == "func":
-            menu[choice]["exec"]()
-        else:
-            presentMenu(menu[choice]["exec"])
-
 def print_matrix():
     matrix = [[1,2,3],[4,5,6],[7,8,9],["*",0,"#"]]
     for x in matrix:
         print(*x)
 
-subMenu = {
-    1: {"display":"f1",
-        "exec":f1,
-        "type":"func"},
-    2: {"display":"f2",
-        "type":"func",
-        "exec":f2,}
-}
+def mainMenu
+  print("[1] option 1")
+  print("[2] option 2")
+  print("[3] option 3")
+  print("[0] Exit")
 
-mainMenu = {
-    1: {"display":"Christmas Tree",
-        "exec":christmastree,
-        "type":"func"},
-    2: {"display":"Ship",
-        "exec":ship,
-        "type":"func"},
-    3: {"display":"Keypad ",
-        "exec":print_matrix,
-        "type":"func"},
-    4: {"display":"Swap ",
-        "exec":swap,
-        "type":"func"},
-    5: {
-        "display":"Submenu",
-        "exec": subMenu,
-        "type":"submenu"
-    }
-}
+mainMenu()
+option = int(input("Enter option"))
 
+while option != 0:
+  if option ==1
+    print("option one")
+  elif option ==2
+    print("option 2")
+  else 
+    print("invalid")
+
+  print()
+  menu()
+  option = int(input("enter option:"))
+
+print("thanks for using")
+  
+   
 if __name__ == "__main__":
     presentMenu(mainMenu)
